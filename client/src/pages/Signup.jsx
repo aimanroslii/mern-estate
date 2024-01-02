fc0523327447823d78c24b7046d48318422d83c4
Aiman Rosli <aimanrosli977@yahoo.com>
2024-01-02 17:33:10 +0800

    Complete sign in page UI and functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -82,7 +82,7 @@ export default function Signup() {
           <span className="text-blue-700"> Sign in</span>
         </Link>
       </div>
-      {/* {error && <p className="text-red-500 mt-5">{error}</p>} */}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
