c121895baad612c985b26d275dbef82501f17e95
Aiman Rosli <aimanrosli977@yahoo.com>
2024-01-02 15:31:51 +0800

    updated complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Malaysia</span>
+            <span className="text-slate-500">Aeyar</span>
             <span className="text-slate-700">Estate</span>
             <span></span>
           </h1>
