2654101c0e5c635d136ef01d23735aa38984c540
Aiman Rosli <aimanrosli977@yahoo.com>
2024-01-18 22:47:30 +0800

    Complete profile page UI and Fix google image URL

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -36,7 +36,7 @@ export default function Header() {
           </Link>
           <Link to="/profile">
             {currentUser ? (
-              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" referrerPolicy='no-referrer'/>
             )
             : (<li className="sm:inline text-slate-700 hover:underline">
             Sign In
