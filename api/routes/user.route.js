8a0eeb746d9ca32f1a0c4d8a35cf0d64e0b06935
Aiman Rosli <aimanrosli977@yahoo.com>
2024-01-21 21:42:04 +0800

    Create update user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { test } from '../controllers/user.controllers.js';
+import { test, updateUser } from '../controllers/user.controllers.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
-const userRouter = express.Router();
+const router = express.Router();
 
-userRouter.get('/test', test);
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser);
 
-export default userRouter;
+export default router;
